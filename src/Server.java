20d2be8cb90758ecef698e789f8eb3c0d519b125
mla387 <mla387@sfu.ca>
2025-03-29 13:19:45 -0700

    penis

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,26 @@
+package player;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
 public class Server {
+	private ArrayList<Player> player_list = new ArrayList<>();	// keeps track of all players
+	private int port_num;
+	private int num_connections = 0;
 
-	public static void main(String[] args){
+	private create_connection() throws IOException {	// creates the server on the port
+		ServerSocket = new ServerSocket(this.port_num);
+		System.out.println("server is running on ", this.port_num);
 
-		System.out.println("Hello, World!");
-	
 	}
+
+	public Server(int port) {
+		this.port_num = port;
+		create_connection();
+		System.out.println("successfully created connection");
+	}
+
+
+
 }
